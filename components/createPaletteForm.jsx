af36358d46d833f7f757115b46ae6b7f90bf3ffc
yyhsieh <923jocelyn@gmail.com>
2019-06-28 19:56:03 +0800

    revise

--- components/createPaletteForm.jsx
+++ components/createPaletteForm.jsx
@@ -1,21 +1,73 @@
 import React, { useState } from 'react'
 // import { useDispatch } from 'react-redux'
-import { Form, Input, Select, Upload, Button, Row, Col, Popover } from 'antd'
+import { Form, Input, Select, Upload, Button, Row, Col, Popover, Icon, Modal } from 'antd'
 import { SketchPicker } from 'react-color'
 const { Item } = Form
 const { Option } = Select
 
+//var data = [1,12,13,4,25,21,22,3,14,5,11,2,23,24,15];
+//var res = skmeans(data,3);
+//console.log(res)
+
 import Palette from '../models/paletteModel.js'
+const skmeans = require("skmeans")
 import { CREATE_PALETTE, PALLETES_QUERY } from '../graphql/index.js';
 import { Mutation } from 'react-apollo'
 
 const OPTIONS_AUTHOR = ['Rainforest']
 const OPTIONS_TAGS = ['Material Design', 'Ant Design', 'Processing', 'Web', 'iOS']
+const BASE64_MARKER = ';base64,';
+
 
 const getBase64 = (img, callback) => {
   const reader = new FileReader()
   reader.addEventListener('load', () => callback(reader.result))
   reader.readAsDataURL(img)
+}
+
+const convertDataURIToBinary = (dataURI) => {
+  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+  var base64 = dataURI.substring(base64Index);
+  var raw = window.atob(base64);
+  var rawLength = raw.length;
+  var array = new Uint8Array(new ArrayBuffer(rawLength));
+  for(var i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
+
+const componentToHex = (c) => {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+const rgbToHex = (r, g, b) => {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+   
+const Kmeans = (url) => {
+  var img_uint8 = convertDataURIToBinary(url)
+  let pivot1 = Math.ceil(img_uint8.length / 3);
+  let pivot2 = Math.ceil(img_uint8.length / 3*2);
+  let pivot3 = Math.ceil(img_uint8.length);
+  let temp_a = img_uint8.slice(0, pivot1);
+  let temp_b = img_uint8.slice(pivot1, pivot2);
+  let temp_c = img_uint8.slice(pivot2, pivot3);
+  var res1 = skmeans(temp_a, 4).centroids
+  var res2 = skmeans(temp_b, 4).centroids
+  var res3 = skmeans(temp_c, 4).centroids
+  var color1 = rgbToHex(Math.ceil(res1[0]), Math.ceil(res2[0]), Math.ceil(res3[0]))
+  var color2 = rgbToHex(Math.ceil(res1[1]), Math.ceil(res2[1]), Math.ceil(res3[1]))
+  var color3 = rgbToHex(Math.ceil(res1[2]), Math.ceil(res2[2]), Math.ceil(res3[2]))
+  var color4 = rgbToHex(Math.ceil(res1[3]), Math.ceil(res2[3]), Math.ceil(res3[3]))
+
+  console.log(color1)
+  console.log(color2)
+  console.log(color3)
+  console.log(color4)
+
+  return [color1, color2, color3, color4]
 }
 
 const CreatePaletteForm = ({showpanel}) => {
@@ -36,16 +88,33 @@ const CreatePaletteForm = ({showpanel}) => {
     setPalette(palette)
   }
 
+  const handleBeforeUpload = file => {
+    const isJPG = file.type === 'image/jpg';
+    const isJPEG = file.type === 'image/jpeg';
+    const isGIF = file.type === 'image/gif';
+    const isPNG = file.type === 'image/png';
+    if (!(isJPG || isJPEG || isGIF || isPNG)) {
+      Modal.error({
+        title: 'Only supported for images',
+      })
+      return 0;
+    };
+    return (isJPG || isJPEG || isGIF || isPNG);
+  };
+
   const uploadImage = info => {
     if(info.file.status === 'uploading'){
       setIsImageUploading(true)
-      return
+      return 
     }
     if(info.file.status === 'done'){
       getBase64(info.file.originFileObj, imgUrl => {
         setIsImageUploading(false)
         console.log(imgUrl)
         palette.image = imgUrl
+        setPalette(palette)
+        var color = Kmeans(imgUrl)
+        palette.colors = [...palette.colors, color[0], color[1], color[2], color[3]]
         setPalette(palette)
       })
     }
@@ -107,11 +176,12 @@ const CreatePaletteForm = ({showpanel}) => {
           className='avatar-uploader'
           action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
           showUploadList={ false }
+          beforeUpload={ handleBeforeUpload }
           onChange={ uploadImage }
         >
           {
             palette.image ? 
-            <img src={ palette.image } alt='avatar'/> :
+            <img src={ palette.image } width='250' alt='avatar'/> :
             <Button
               icon={ isImageUploading ? 'loading' : 'plus' }
             >
